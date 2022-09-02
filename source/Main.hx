78410b1bc868639eb69d29282dbd6f8a9b9a7c72
That1LazerBoi <giobonnie1247@gmail.com>
2022-09-02 01:25:48 -0500

    hoh.

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,6 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, PlayState));
+		addChild(new FlxGame(0, 0, PlayState, 1, #if html5 60, 60 #else 120, 120 #end, true, false));
 	}
 }
